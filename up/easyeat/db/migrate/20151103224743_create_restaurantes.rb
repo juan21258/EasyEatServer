5440b6a07f21ca6f98b9ccec4ea56db0784ef1e0
Sergio AC <shergio0312@gmail.com>
2015-11-05 17:14:54 -0500

    A la mierda Rails

--- up/easyeat/db/migrate/20151103224743_create_restaurantes.rb
+++
@@ -1,9 +0,0 @@
-class CreateRestaurantes < ActiveRecord::Migration
-  def change
-    create_table :restaurantes do |t|
-      t.string :descripcion
-
-      t.timestamps null: false
-    end
-  end
-end
--- up/easyeat/test/models/restaurante_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class RestauranteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
