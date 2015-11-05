12bebf45696aeb9916ca6252d771e08c295d5105
Santiago Montoya <santyxdz@gmail.com>
2015-11-05 16:37:48 -0500

    Primer Commit

---
+++ src/main/java/com/saprykin/heroku_spark/App.java
@@ -0,0 +1,32 @@
+package com.saprykin.heroku_spark;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.sql.*;
+
+import static spark.Spark.*;
+
+// heroku auth:token
+
+public class App {
+
+    public static void main(String[] args) {
+
+        //Heroku assigns different port each time, hence reading it from process.
+        ProcessBuilder process = new ProcessBuilder();
+        Integer port;
+        if(process.environment().get("PORT") != null) {
+            port = Integer.parseInt(process.environment().get("PORT"));
+        } else {
+            port = 8080;
+        }
+        setPort(port);
+
+        DaoTest daoTest = new DaoTest();
+        final String dbTestString = daoTest.testDb();
+
+        get("/hello", (request, response) -> "<html><head><h1>Hello World!</h1></head><body>" + "<h2>" + dbTestString + "</h2>" + "</body></html>");
+    }
+
+
+}
