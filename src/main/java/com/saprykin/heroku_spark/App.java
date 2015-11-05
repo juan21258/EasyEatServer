729f99d93f744179885d6a69d003580bf34c02f2
Sergio AC <shergio0312@gmail.com>
2015-11-05 17:52:06 -0500

    API

--- src/main/java/com/saprykin/heroku_spark/App.java
+++ src/main/java/com/saprykin/heroku_spark/App.java
@@ -30,14 +30,15 @@ public class App {
         //DaoTest daoTest = new DaoTest();
         //final String dbTestString = daoTest.testDb();
         ArrayList<String> datos = new ArrayList<>();
-        get("/", (request, response) -> "<html><head><h1>Hello World!</h1></head><body bgcolor=\"#e34234\">"
+        get("/", (request, response) -> "<html><head><h1>API: EasyEat)</h1></head><body bgcolor=\"#e34234\">"
             + "<h2>" + "</h2>" + "</body></html>");
         post("/echo", (request, response) -> {
             datos.add(request.queryParams("text"));
             return "";
         });
         get("/echo", (request, response) -> {
-            String aux = "<center><h1>Pedidos:</h1></center> <br/>";
+            String aux = "<html><head><h1>API: EasyEat)</h1></head><body bgcolor=\"#e34234\">"
+            + "<h1>" +"Pedidos" + "</h1>" + "</body></html>";
             for(String text:datos){
                 aux+=(text+"<br/>\n");
             }
