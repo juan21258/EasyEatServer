93b26386593b945531635a2ae5baa82355fa4542
Julián Rincón <jrincon5@eafit.edu.co>
2015-11-13 22:51:08 -0500

    a

---
+++ src/main/java/co/edu/eafit/easyeatserver/Entidades/CatalogoEnt.java
@@ -0,0 +1,7 @@
+package co.edu.eafit.easyeat.Entidades;
+
+/**
+ * Created by Julian on 12/11/2015.
+ */
+public class CatalogoEnt{
+}
---
+++ src/main/java/co/edu/eafit/easyeatserver/Entidades/PedidoEnt.java
@@ -0,0 +1,52 @@
+package co.edu.eafit.easyeat.Entidades;
+
+/**
+ * Created by Julian on 12/11/2015.
+ */
+
+public class PedidoEnt {
+    private int id_Pedido;
+    private double tiempoEspera;
+    private int totalPagar;
+    private int cantidad;
+    private ProductoEnt producto;
+
+    public PedidoEnt(int id_Pedido, double tiempoEspera, int totalPagar, int cantidad) {
+        this.id_Pedido = id_Pedido;
+        this.tiempoEspera = tiempoEspera;
+        this.totalPagar = totalPagar;
+        this.cantidad = cantidad;
+    }
+
+    public int getId_Pedido() {
+        return id_Pedido;
+    }
+
+    public void setId_Pedido(int id_Pedido) {
+        this.id_Pedido = id_Pedido;
+    }
+
+    public double getTiempoEspera() {
+        return tiempoEspera;
+    }
+
+    public void setTiempoEspera(int tiempoEspera) {
+        this.tiempoEspera = tiempoEspera;
+    }
+
+    public int getTotalPagar() {
+        return totalPagar;
+    }
+
+    public void setTotalPagar(int totalPagar) {
+        this.totalPagar = totalPagar;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+}
